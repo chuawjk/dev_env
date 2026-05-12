b3505963c9be15610abdd9915afe3921d4ea522b
Kenny Chua <chuawjk@gmail.com>
2026-05-12 15:37:40 +0800

    Add installer

---
+++ install.sh
@@ -0,0 +1,17 @@
+# Homebrew (if not already installed)
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# Zsh plugins
+brew install zsh-autosuggestions zsh-syntax-highlighting
+
+# Git
+brew install git
+
+# GNU ls (color support)
+brew install coreutils
+
+# Miniconda
+brew install --cask miniconda
+
+# uv
+brew install uv
